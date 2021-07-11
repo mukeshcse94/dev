8626697bead87c81a3688edd3271a17ddc642a01
mukesh <mukese0794@gmail.com>
2021-07-11 19:09:19 +0530

    tour of heros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,9 @@
 import { Component } from '@angular/core';
-import { Hero } from 'src/app/hero';
-import { HOROS } from './mocks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'demo';
-  
-  heroes = HOROS;
-  
-  selectedHero? : Hero;  
-  onSelect(hero: Hero):void {
-     this.selectedHero = hero
-  }
+  title = 'Tour of Heroes';
 }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,6 +1,6 @@
-import { Hero } from './hero'
+import { Hero } from './hero';
 
-export const HOROS: Hero[]=[
+export const HEROES: Hero[] = [
   { id: 11, name: 'Dr Nice' },
   { id: 12, name: 'Narco' },
   { id: 13, name: 'Bombasto' },
@@ -11,4 +11,4 @@ export const HOROS: Hero[]=[
   { id: 18, name: 'Dr IQ' },
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' }
-]
+];
