3e002b41703760d50e9fd564d85dba25ecb271c2
mukesh <mukese0794@gmail.com>
2021-07-11 15:27:10 +0530

    single page 3. event binding (Select one user details from list)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Hero } from 'src/app/hero';
+import { HOROS } from './mocks';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo';
+  
+  heroes = HOROS;
+  
+  selectedHero? : Hero;  
+  onSelect(hero: Hero):void {
+     this.selectedHero = hero
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HerosComponent } from './components/heros/heros.component';
-import { HerosDetailsComponent } from './components/heros-details/heros-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HerosComponent,
-    HerosDetailsComponent
   ],
   imports: [
     BrowserModule,
